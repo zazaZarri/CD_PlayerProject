3996625b262aec6b03b69dc3887d7732468335cb
zarri <nikozarro@gmail.com>
2026-05-25 16:05:54 +0200

    Added funtionalities

--- CD_PlayerProject/CD.cs
+++ CD_PlayerProject/CD.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace CD_PlayerProject
 {
-    internal class CD
+    public class CD
     {
+
+        public string Titolo { get; set; }
+        public List<Brano> Brani { get; set; }
+
+        public Autore autore { get; set; }
+
+        public CD(string Titolo, List<Brano> Brani, Autore autore)
+        {
+            this.Titolo = Titolo;
+            this.Brani = Brani;
+            this.autore = autore;
+        }
+
+
+        public string getTitolo()
+        {
+            return Titolo;
+        }
+
+        public List<Brano> getBrani()
+        {
+            return Brani;
+        }
+
+
+        public Autore getAutore()
+        {
+            return autore;
+        }
+
+
+
+        public void setTitolo(string Titolo)
+        {
+            this.Titolo = Titolo;
+        }
+
+        public void setAutore(Autore autore)
+        {
+            this.autore = autore;
+        }
     }
 }
