b9e836d9cc602d57f81d053b8d3d2facb2a3c67b
checco <francesco.biguzzi09@gmail.com>
2026-05-25 15:52:33 +0200

    adde functionalities

--- CD_PlayerProject/Brano.cs
+++ CD_PlayerProject/Brano.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace CD_PlayerProject
 {
-    internal class Brano
+    public class Brano
     {
+        public string Titolo { get; set; }
+        public int Durata_S { get; set; }
+        public Autore autore { get; set; }
+        public Brano(string Titolo, int Durata_s, Autore autore)
+        {
+            this.Titolo = Titolo;
+            this.Durata_S = Durata_s;
+            this.autore = autore;
+        }
+        public string getTitolo()
+        {
+            return Titolo;
+        }
+        public Autore getAutore()
+        {
+            return autore;
+        }
+        public int getDurata()
+        {
+            return Durata_S;
+        }
+
+        public override string ToString()
+        {
+            return $"Titolo: {Titolo} - Autore: {autore} - Durata: {Durata_S}";
+        }
+        public bool shortSong(int x)
+        {
+            if (x <= 0)
+            {
+                throw new ArgumentException("iol valore deve essere positivo");
+            }
+            if (Durata_S <= x)
+            {
+                return true ;
+            }
+            else
+            {
+                return false ;
+            }
+        }
+
     }
 }
