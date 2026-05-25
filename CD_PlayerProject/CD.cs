945fd0a20c3588e6c1693d47c2811e1c90c37732
zarri <nikozarro@gmail.com>
2026-05-25 16:01:20 +0200

    Added CD CLASS

---
+++ CD_PlayerProject/CD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD_PlayerProject
+{
+    internal class CD
+    {
+    }
+}
