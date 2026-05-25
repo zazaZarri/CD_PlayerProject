9edf74167a5ffa524583ea5fdfb8acb42dd88a55
zarri <nikozarro@gmail.com>
2026-05-25 15:05:52 +0200

    Added Autore in features

---
+++ CD_PlayerProject/Autore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD_PlayerProject
+{
+    public class Autore
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Eta { get; set; }
+
+        public Autore(string nome, string cognome, int eta)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Eta = eta;
+        }
+
+
+        public string GetFullName()
+        {
+            return $"{Nome} {Cognome}";
+        }
+
+        public int GetEta()
+        {
+            return Eta;
+        }
+    }
+}
